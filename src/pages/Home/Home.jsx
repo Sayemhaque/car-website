4cf6d41240269d95538bc7580f9f9bebb7db5868
Mohammad sayem Haque <85791327+Sayemhaque@users.noreply.github.com>
2023-10-01 18:14:48 +0600

    redux connected

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
+import { useGetPokemonByNameQuery } from "../../Redux/services/baseApi";
 import Header from "../../components/Header/Header";
 
 const Home = () => {
+    const {data:pro} = useGetPokemonByNameQuery()
+    console.log(pro)
     return (
         <section className="bg-indigo-100 h-screen">
             <Header/>
